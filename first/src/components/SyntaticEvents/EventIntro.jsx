e9efdddefc7887fe5e39116db77d0bdca787dc7f
Shubham <chaudhary2001shubham@gmail.com>
2024-07-06 14:11:54 +0530

    Syntatic Events

---
+++ first/src/components/SyntaticEvents/EventIntro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import EventEx1 from './EventEx1'
+import EventEx2 from './EventEx2'
+import EventEx3 from './EventEx3'
+
+function EventIntro() {
+  return (
+    <div>
+        {/* <EventEx1/> */}
+        {/* <EventEx2/> */}
+        <EventEx3/>
+    </div>
+  )
+}
+
+export default EventIntro
